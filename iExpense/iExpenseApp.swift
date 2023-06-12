618d803763977c3709f3ff116ab1cbab8d3742fb
LuiArm <Luisarmendariz24@icloud.com>
2023-06-11 22:26:54 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by luis armendariz on 6/11/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
