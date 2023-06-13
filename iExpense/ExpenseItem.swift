9eda106cd68743eed2cce442391cab35361df46d
LuiArm <Luisarmendariz24@icloud.com>
2023-06-12 23:54:17 -0600

    Adding expense item struct

---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,15 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by luis armendariz on 6/12/23.
+//
+
+import Foundation
+
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
